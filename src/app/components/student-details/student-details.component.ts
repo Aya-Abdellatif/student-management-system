8d789f44343f8271899be1280b3c769fa90c0bf5
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-04-19 04:18:57 +0200

    Update student-table

--- src/app/components/student-details/student-details.component.ts
+++ src/app/components/student-details/student-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StudentsService } from 'src/app/services/students.service';
 
 @Component({
   selector: 'app-student-details',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-details.component.css']
 })
 export class StudentDetailsComponent {
+  student: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private _studentsService: StudentsService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.student = this._studentsService.getStudentByID(id);
+  }
 
 }
