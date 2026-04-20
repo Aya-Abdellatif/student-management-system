f033c95f5c56ae0e2acf7ec65a3ae56122231066
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-04-20 02:50:42 +0200

    Apply Filtration in logic

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StudentsService } from 'src/app/services/students.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-students',
@@ -8,12 +9,32 @@ import { StudentsService } from 'src/app/services/students.service';
 })
 export class StudentsComponent {
 
-  constructor(private _studentsService: StudentsService){}
-
+  constructor(private _studentsService: StudentsService) { }
   students$ = this._studentsService.students$;
 
-  deleteStudent(id: number){
+  deleteStudent(id: number) {
     this._studentsService.deleteStudent(id);
+  }
+
+  searchName: string = '';
+  selectedDepartment: string = '';
+
+  applyFilter() {
+    return this.students$ = this._studentsService.students$.pipe(
+      map(students => students.filter(s => {
+        const fullName = (s.firstName + ' ' + s.lastName).toLowerCase();
+        console.log(fullName);
+        
+        const matchesSearch =
+          fullName.includes(this.searchName.toLowerCase());
+
+        const matchesDepartment =
+          this.selectedDepartment === '' ||
+          s.department === this.selectedDepartment;
+
+        return matchesSearch && matchesDepartment;
+      }))
+    );
   }
 
 }
