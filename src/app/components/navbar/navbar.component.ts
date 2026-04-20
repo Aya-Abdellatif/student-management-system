4ae8e2d1fa6b572d59af540edd8f76a32b13ce3b
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-04-20 16:41:07 +0200

    Add bg-color for mobile navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  closeMenu(): void {
+    const navbarCollapse = document.getElementById('navbarMain');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+    }
+  }
 
 }
